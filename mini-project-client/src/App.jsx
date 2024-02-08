28f8713e83482ce46c0e4577db8df65f71a9bf92
AtulShince27 <atulk.shince@gmail.com>
2024-02-08 15:54:15 +0530

    first commit

---
+++ mini-project-client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        <h1 className="text-3xl font-bold">
+          Hello world!
+          <p>
+            Welcome to Mini Project 2024
+          </p>
+        </h1>
+    </div>
+  )
+}
+
+export default App
