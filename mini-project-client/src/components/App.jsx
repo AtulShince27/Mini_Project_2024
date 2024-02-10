09fa34ec6164f934d6bee8e49e84f9807c771d35
uwu_rimjhim <rimjhimshrivastava19@gmail.com>
2024-02-10 16:25:19 +0530

    Navbar component created

--- mini-project-client/src/components/App.jsx
+++ mini-project-client/src/components/App.jsx
@@ -1,18 +1,13 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import '../styles/App.scss'
+import Navbar from './Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-        <h1 className="text-3xl font-bold">
-          Hello world!
-          <p>
-            Welcome to Mini Project 2024
-          </p>
-        </h1>
+    <div className='app'>
+        <Navbar />
     </div>
   )
 }
