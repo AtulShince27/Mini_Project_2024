836d7c13ad2ea99106a273c49999d2eeaaa52013
hornycompiler <Talktotanmoy.k@gmail.com>
2024-02-17 09:54:36 +0530

    Login Page added

--- mini-project-client/src/pages/LoginPage.jsx
+++ mini-project-client/src/pages/LoginPage.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import './LoginPage.scss'
+import loginImage from '../assets/login_page_illustration.png'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className= "login-page">
+      <div className="main">
+          <div className="content">
+            <div className="heading">
+              <h1>Welcome Back!</h1>
+            </div>
+            <div className="heading2">
+              <h6>Please Login to your account</h6>
+            </div>
+            <form className='login-form'>
+              <div>
+                <input type="text" id="username" name="username" className='inputDiv' placeholder='Username'/>
+              </div>
+              <div>
+                <input type="password" id="password" name="password" className='inputDiv' placeholder='Password'/>
+              </div >
+              <div className='forgot'><a href="#">Forgot Password?</a></div>
+              <div className='buttons'>
+                <button type="submit" className='Btn' id="loginBtn">Login</button>
+                <button className='Btn' id='signinBtn'>Signin</button>
+              </div>
+            </form>
+        </div>
+        <div class="image"> </div>
+      </div>
+    </div>
   )
 }
 
